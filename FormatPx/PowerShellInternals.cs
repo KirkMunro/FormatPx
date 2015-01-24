074d5d68339c206935f35e24a286c475c03c283b
Kirk Munro <KirkMunro@users.noreply.github.com>
2015-01-24 15:13:12 -0500

    fixed bug surfaced by an internal change in PSv5 that prevented the hiding of the __formatdata note property in that version; incremented version number of module

--- FormatPx/PowerShellInternals.cs
+++ FormatPx/PowerShellInternals.cs
@@ -16,10 +16,10 @@ namespace FormatPx
             // extension method can be included in any project where it is needed.
 
             // Look up the GetSteppablePipeline internal method
-            MethodInfo getSteppablePipelineMethod = typeof(PowerShell).GetMethod("GetSteppablePipeline", BindingFlags.NonPublic | BindingFlags.Instance, null, Type.EmptyTypes, null);
+            MethodInfo getSteppablePipelineMethod = typeof(PowerShell).GetMethod("GetSteppablePipeline", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, null, Type.EmptyTypes, null);
             if (getSteppablePipelineMethod == null)
             {
-                MethodAccessException exception = new MethodAccessException("Failed to find internal GetSteppablePipeline method.");
+                MethodAccessException exception = new MethodAccessException("Failed to find GetSteppablePipeline method.");
                 ErrorRecord errorRecord = new ErrorRecord(exception, exception.GetType().FullName, ErrorCategory.ResourceUnavailable, ps);
                 psCmdlet.ThrowTerminatingError(errorRecord);
             }
@@ -38,17 +38,32 @@ namespace FormatPx
             // transparent. Better to just hide the field altogether and make sure that it
             // doesn't show up in the output at all.
 
-            // Look up the isHidden internal field
-            FieldInfo isHiddenField = typeof(PSNoteProperty).GetField("isHidden", BindingFlags.NonPublic | BindingFlags.Instance);
+            // Look up the isHidden value, first using the internal field, then using the property
+            FieldInfo isHiddenField = typeof(PSNoteProperty).GetField("isHidden", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
             if (isHiddenField == null)
             {
-                FieldAccessException exception = new FieldAccessException("Failed to find internal PSNoteProperty field.");
-                ErrorRecord errorRecord = new ErrorRecord(exception, exception.GetType().FullName, ErrorCategory.ResourceUnavailable, psNoteProperty);
-                psCmdlet.ThrowTerminatingError(errorRecord);
+                // In PSv5+, the isHidden field is not accessible this way, but the IsHidden property
+                // is so when the field is not found, we try using the property instead. We don't use
+                // the property first because the property does not have a set accessor in v3 or v4.
+                PropertyInfo isHiddenProperty = typeof(PSNoteProperty).GetProperty("IsHidden", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                if (isHiddenProperty == null)
+                {
+                    // When neither the field nor the property can be found, raise an exception.
+                    FieldAccessException exception = new FieldAccessException("Failed to find isHidden field.");
+                    ErrorRecord errorRecord = new ErrorRecord(exception, exception.GetType().FullName, ErrorCategory.ResourceUnavailable, psNoteProperty);
+                    psCmdlet.ThrowTerminatingError(errorRecord);
+                }
+                else
+                {
+                    // Now hide the PSNoteProperty
+                    isHiddenProperty.SetValue(psNoteProperty, true);
+                }
             }
-
-            // Now hide the PSNoteProperty
-            isHiddenField.SetValue(psNoteProperty, true);
+            else
+            {
+                // Now hide the PSNoteProperty
+                isHiddenField.SetValue(psNoteProperty, true);
+            }
         }
     }
 }
